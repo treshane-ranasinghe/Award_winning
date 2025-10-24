7a8aed2a7fea04387c01e543a18a26e2aaf4563c
treshane-ranasinghe <treshaneranasinghe@outlook.com>
2025-10-25 00:59:19 +0530

    font & color scheme setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const App = () => {
   return (
     <main>
-      <h1 className="text-3xl font-bold font-robert-medium underline">
+      <h1 className="text-3xl text-violet-300 font-bold font-robert-medium underline">
       Hello world!
     </h1>
     </main>
